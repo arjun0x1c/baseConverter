3864497808e85df6a6faf9ce22d77670913757bc
arjun0x1c <vimalarjun28@gmail.com>
2023-09-25 17:23:13 +0530

    Full version

--- script.js
+++ script.js
@@ -14,12 +14,7 @@ function decimalConverter(val, base) {
 
 function baseToDecimal(val, base) {
     let decimalValue = 0;
-    if(base === 2) {
-        for(let i=val.length -1, j=0; i>=0; i--, j++) {
-            decimalValue += parseInt(val[i]) * Math.pow(base, j);
-        }
-    }
-    else if(base === 8) {
+    if(base === 2 || base === 8) {
         for(let i=val.length -1, j=0; i>=0; i--, j++) {
             decimalValue += parseInt(val[i]) * Math.pow(base, j);
         }
@@ -27,7 +22,7 @@ function baseToDecimal(val, base) {
     else if(base === 16) {
         for(let i=val.length -1, j=0; i>=0; i--, j++) {
             if(isNaN(val[i])) {
-                decimalValue += ((val[i].charCodeAt() - 65)+10) * Math.pow(base, j);
+                decimalValue += ((val[i].toUpperCase().charCodeAt() - 65)+10) * Math.pow(base, j);
             }
             else {
             decimalValue += parseInt(val[i]) * Math.pow(base, j);
@@ -41,45 +36,52 @@ function baseToDecimal(val, base) {
 }
 
 function validation(val, base) {
-    flag = true;
+    let flag = true;
     base = parseInt(base);
 
     if(base === 2) {
-        for(let i=0; i<val.length; i++) {
-            if(!(parseInt(val[i]) === 1 || parseInt(val[i]) === 0)){
+        for(let i of val) {
+            if(!(parseInt(i) === 1 || parseInt(i) === 0)){
                 flag = false;
                 break;
             }
         }
     }
     else if(base === 8) {
-        for(let i=0; i<val.length; i++) {
-            if(!(parseInt(val[i]) >= 0 && parseInt(val[i]) <= 8)){
+        for(let i of val) {
+            if(!(parseInt(i) >= 0 && parseInt(i) <= 8)){
                 flag = false;
                 break;
             } 
         }
     }
     else if(base === 16) {
-        for(let i=0; i<val.length; i++) {
-            if(!(parseInt(val[i]) >= 0 && parseInt(val[i]) <= 9 || val[i].charCodeAt() >= 65 && val[i].charCodeAt() <= 70)) {
+        for(let i of val) {
+            i = i.toUpperCase();
+            if(!(parseInt(i) >= 0 && parseInt(i) <= 9 || i.charCodeAt() >= 65 && i.charCodeAt() <= 70)) {
                 flag = false;
                 break;
             }
         }
     }
     else if(base === 10) {
-        return !isNaN(val);
+        return !isNaN(val) && val >= 0;
     }
     return flag;
 }
-
 
 const VALUE = _("value");
 const BASE = _("option1");
 const ERROR_BOX = _("error-msg");
 const RESULT = _("result");
 const BASE2 = _("option2");
+
+function clearr() {
+    console.log("doing");
+    VALUE.value = "";
+    RESULT.textContent = "";
+    ERROR_BOX.innerHTML = "";
+}
 
 function check() {
     placeholder();
@@ -92,11 +94,23 @@ function check() {
 }
 
 function placeholder() {
-    let base = BASE.VALUE;
+    let base = parseInt(BASE.value);
+    console.log(base);
     if (base === 2) {
         VALUE.placeholder = "100101";
+    } else if (base === 8) {
+        VALUE.placeholder = "1280";
+    } else if (base === 16) {
+        VALUE.placeholder = "35F";
+    } else {
+        VALUE.placeholder = "1234";
     }
 }
+
 function main() {
+    if (!VALUE.value) {
+        ERROR_BOX.innerHTML = "<b>Input can't be empty.</b>";
+        return;
+    }
     RESULT.innerHTML = VALUE.value != 0 ? decimalConverter(baseToDecimal(VALUE.value, +BASE.value), +BASE2.value) : 0;
 }
